86ccb3ed0c061fe2c37c66b6f6a2f25eb5c0488d
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2024-08-08 21:54:42 +0200

    Expression IR

--- midend/ir.hpp
+++ midend/ir.hpp
@@ -28,7 +28,15 @@ enum class IRType {
     STATEMENT,
 
     MODULE,
-    SPACE
+    SPACE,
+
+    BINARY_EXPR,
+    VARIABLE,
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    BOOL_LITERAL,
+    STRING_LITERAL,
+    NIL_LITERAL
 };
 
 /** Base class for any IR value */
@@ -118,6 +126,131 @@ public:
 
     virtual inline std::ostream& debug(std::ostream& os) const {
         os << "space " << name;
+        return os;
+    }
+};
+
+/**
+ * Types of operators
+ */
+enum OperatorKind {
+    OP_CONCAT, ///< ++
+    OP_EXP,    ///< ^
+    OP_PLUS,   ///< `+`
+    OP_MINUS,  ///< `-`
+    OP_DIV,    ///< /
+    OP_MUL,    ///< `*`
+    OP_MOD,    ///< %
+    OP_SET,    ///< =
+    OP_SILENT, ///< ~
+    OP_EQ,     ///< ==
+    OP_NEQ,    ///< !=
+    OP_BT,     ///< `>`
+    OP_LT,     ///< <
+    OP_BEQ,    ///< >=
+    OP_LEQ,    ///< <=
+    OP_SHORT_C_AND, ///< &&
+    OP_SHORT_C_OR,  ///< ||
+    OP_AND,    ///< and
+    OP_OR,     ///< or
+    OP_NOT,    ///< not
+    OP_XOR,    ///< xor
+    OP_IN,     ///< in
+    OP_SLICE,  ///< [..]
+    OP_ACCESS, ///< `.`
+    OP_SUBSC,  ///< []
+
+    //OP_UNKNOWN
+};
+
+/**
+ * Operator in an expression
+ */
+class Operator {
+private:
+    OperatorKind kind;
+public:
+    Operator(OperatorKind kind) : kind(kind) {}
+
+    OperatorKind getKind() const { return kind; }
+
+    inline std::ostream& debug(std::ostream& os) const { 
+        switch(kind) {
+        case OP_CONCAT: os << "++"; break;
+        case OP_EXP: os << "^"; break;
+        case OP_PLUS: os << "+"; break;
+        case OP_MINUS: os << "-"; break;
+        case OP_DIV: os << "/"; break;
+        case OP_MUL: os << "*"; break;
+        case OP_MOD: os << "%"; break;
+        case OP_SET: os << "="; break;
+        case OP_SILENT: os << "~"; break;
+        case OP_EQ: os << "=="; break;
+        case OP_NEQ: os << "!="; break;
+        case OP_BT: os << ">"; break;
+        case OP_LT: os << "<"; break;
+        case OP_BEQ: os << ">="; break;
+        case OP_LEQ: os << "<="; break;
+        case OP_SHORT_C_AND: os << "&&"; break;
+        case OP_SHORT_C_OR: os << "||"; break;
+        case OP_AND: os << "and"; break;
+        case OP_OR: os << "or"; break;
+        case OP_NOT: os << "not"; break;
+        case OP_XOR: os << "xor"; break;
+        case OP_IN: os << "in"; break;
+        case OP_SLICE: os << "[..]"; break;
+        case OP_ACCESS: os << "."; break;
+        case OP_SUBSC: os << "[]"; break;
+        //case OP_UNKNOWN: os << "unknown"; break;
+        default:
+            assert(false && "Missing operator in debug for Operator class"); 
+            os << "unknown operator"; break;
+        }
+        return os;
+    }
+};
+
+inline std::ostream& operator<< (std::ostream& os, const Operator &op) {
+    return op.debug(os);
+}
+
+class BinaryExpr : public Expression {
+private:
+    Expression *left;
+    Expression *right;
+    const Operator op;
+public:
+    BinaryExpr(Expression *left, Expression *right, Operator op) 
+              : Expression(IRType::BINARY_EXPR, "<binary-expression>"),
+                left(left), right(right), op(op) {}
+
+    Expression *get_left() { return this->left; }
+    Expression *get_right() { return this->right; }
+
+    virtual inline std::ostream& debug(std::ostream& os) const {
+        os << *left << " " << op << " " << *right;
+        return os;
+    }
+};
+
+class Variable : public Expression {
+public:
+    Variable(ustring name) : Expression(IRType::VARIABLE, name) {}
+
+    virtual inline std::ostream& debug(std::ostream& os) const {
+        os << name;
+        return os;
+    }
+};
+
+class IntLiteral : public Expression {
+private:
+    opcode::IntConst value;
+public:
+    IntLiteral(opcode::IntConst value) : Expression(IRType::INT_LITERAL, "<int-literal>"), value(value) {}
+
+    virtual inline std::ostream& debug(std::ostream& os) const {
+        os << value;
         return os;
     }
 };
