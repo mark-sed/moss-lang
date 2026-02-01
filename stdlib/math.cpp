f7746a3cca04155a0a45a68e3451b2d755f9b514
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2026-02-01 19:28:45 +0100

    math.isnan, math.isinf, test to hyperbolics test to use isnan, removed old run-tests.sh

--- stdlib/math.cpp
+++ stdlib/math.cpp
@@ -7,6 +7,12 @@ using namespace math;
 
 const std::unordered_map<std::string, mslib::mslib_dispatcher>& math::get_registry() {
     static const std::unordered_map<std::string, mslib::mslib_dispatcher> registry = {
+        {"atan", [](Interpreter*, CallFrame* cf, Value*&) {
+            return FloatValue::get(std::atan(cf->get_args()[0].value->as_float()));
+        }},
+        {"atan2", [](Interpreter*, CallFrame* cf, Value*&) {
+            return FloatValue::get(std::atan2(cf->get_arg("x")->as_float(), cf->get_arg("y")->as_float()));
+        }},
         {"cosh", [](Interpreter*, CallFrame* cf, Value*&) {
             return FloatValue::get(std::cosh(cf->get_args()[0].value->as_float()));
         }},
@@ -16,11 +22,8 @@ const std::unordered_map<std::string, mslib::mslib_dispatcher>& math::get_regist
         {"tanh", [](Interpreter*, CallFrame* cf, Value*&) {
             return FloatValue::get(std::tanh(cf->get_args()[0].value->as_float()));
         }},
-        {"atan", [](Interpreter*, CallFrame* cf, Value*&) {
-            return FloatValue::get(std::atan(cf->get_args()[0].value->as_float()));
-        }},
-        {"atan2", [](Interpreter*, CallFrame* cf, Value*&) {
-            return FloatValue::get(std::atan2(cf->get_arg("x")->as_float(), cf->get_arg("y")->as_float()));
+        {"isnan", [](Interpreter*, CallFrame* cf, Value*&) {
+            return BoolValue::get(std::isnan(cf->get_args()[0].value->as_float()));
         }},
     };
     return registry;
